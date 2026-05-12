e68a3ac90ab53d02e61d728e5b61875f7ce03f0d
Lucas Denton <ladent99@gmail.com>
2026-05-11 22:37:46 -0400

    chore(gateway): remove ReadingsGateway from providers until listen/notify is hardened

--- src/readings/readings.module.ts
+++ src/readings/readings.module.ts
@@ -1,11 +1,10 @@
 import { Module } from "@nestjs/common";
 
 import { ReadingsController } from "./readings.controller";
-import { ReadingsGateway } from "./readings.gateway";
 import { ReadingsService } from "./readings.service";
 
 @Module({
   controllers: [ReadingsController],
-  providers: [ReadingsService, ReadingsGateway],
+  providers: [ReadingsService],
 })
 export class ReadingsModule {}
